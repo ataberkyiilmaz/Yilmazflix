cabd39f8e30a9d287aca8e878b2c6e8034358720
Ata Berk Yılmaz <69900743+ataberkyiilmaz@users.noreply.github.com>
2022-07-17 00:23:27 +0300

    some changes for dynamic data table

--- user_management.php
+++ user_management.php
@@ -14,13 +14,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User Management</title>
-    <link rel="stylesheet" type="text/css" href="css/layout_styles.css?<?=filemtime('css/layout_styles.css');?>">
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap4.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap4.min.css">
 </head>
 <body>
 
 <h1 class="header">User Management</h1>
 
-<table class="tb">
+<table id = "example" class="table table-striped table-bordered" style="width:100%">
     <thead>
         <tr>
             <th class="col">id</th>
@@ -49,7 +53,11 @@
 
         }
 
+        echo '</tbody>
+        ';
+
     ?>
 
+<script src="./js/script2.js"></script>
 </body>
 </html>
